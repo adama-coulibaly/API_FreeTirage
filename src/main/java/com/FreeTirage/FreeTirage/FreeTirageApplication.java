27cc673160f24113565354e2d39bcba854f53983
adcoulibaly <coulibalyadamabekaye03@gmail.com>
2022-08-18 16:12:48 +0000

    Premier depot

---
+++ src/main/java/com/FreeTirage/FreeTirage/FreeTirageApplication.java
@@ -0,0 +1,13 @@
+package com.FreeTirage.FreeTirage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTirageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeTirageApplication.class, args);
+	}
+
+}
